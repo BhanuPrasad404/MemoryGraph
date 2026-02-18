a43896b9f269d1314ea36ba0e33f99e5117940b0
BhanuPrasad404 <bhanuprasadgummidelli@gmail.com>
2026-02-18 16:03:51 +0530

    Fix wildcard route syntax

--- server.js
+++ server.js
@@ -87,7 +87,7 @@ app.use(cors({
   methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization", "X-Requested-With", "X-Client-Data"]
 }));
-app.options("*", cors());
+app.options("/*", cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
